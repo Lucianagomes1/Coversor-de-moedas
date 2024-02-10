c9cf3fab26c915e200f0b6dd23449d4c7fdf75e8
Luciana Gomes <lucianasousa@uninove.edu.br>
2024-02-10 12:35:54 -0300

    Valores com API

--- scripts.js
+++ scripts.js
@@ -1,58 +1,66 @@
-const convertbutton = document.querySelector(".butto");
-const selecteuro = document.querySelector(".Euro");
+const convertButton = document.querySelector(".butto");
+const selectEuro = document.querySelector(".Euro");
 
 function ConvertValor() {
-  const valorinput = document.querySelector(".value").value;
+  const valorInput = document.querySelector(".value").value;
 
-  const valor_real = document.querySelector(".par2");
+  const valorReal = document.querySelector(".par2");
+  const valorDolar = document.querySelector(".par3");
 
-  const valor_dolar = document.querySelector(".par3");
+  let dolar = 5.2;  
+  let euro = 6.2;   
 
-  console.log(selecteuro.value);
+  
+  const apiKey = '949dc37b7ae33ea7d587f985';
 
-  const dolar = 5.2;
+  fetch(`https://open.er-api.com/v6/latest/USD?apikey=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+     
+      dolar = data.rates.BRL;
+      euro = data.rates.EUR;
 
-  const euro = 6.2;
+      if (selectEuro.value === "dolar") {
+        valorDolar.innerHTML = new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(valorInput / dolar);
+      }
 
-  if (selecteuro.value == "dolar") {
-    valor_dolar.innerHTML = new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-    }).format(valorinput / dolar);
-  }
+      if (selectEuro.value === "euro") {
+        valorDolar.innerHTML = new Intl.NumberFormat("de-DE", {
+          style: "currency",
+          currency: "EUR",
+        }).format(valorInput / euro);
+      }
 
-  if (selecteuro.value == "euro") {
-    valor_dolar.innerHTML = new Intl.NumberFormat("de-DE", {
-      style: "currency",
-      currency: "EUR",
-    }).format(valorinput / euro);
-  }
-
-  valor_real.innerHTML = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  }).format(valorinput);
+      valorReal.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(valorInput);
+    })
+    .catch(error => {
+      console.error("Erro ao obter as taxas de câmbio:", error);
+      
+    });
 }
 
-function trocadevalor(){
-    const bandeira = document.querySelector(".par4");
-    const trocabandeira = document.querySelector(".imagem1");
+function trocaDeValor() {
+  const bandeira = document.querySelector(".par4");
+  const trocaBandeira = document.querySelector(".imagem1");
 
+  if (selectEuro.value === "dolar") {
+    bandeira.innerHTML = "Dólar Americano";
+    trocaBandeira.src = "./assets/estados-unidos (1) 1.png";
+  }
 
-    if (selecteuro.value == "dolar"){
-        bandeira.innerHTML = "Dolar Americano";
-       trocabandeira.src = "./assets/estados-unidos (1) 1.png";
-    
-    }
+  if (selectEuro.value === "euro") {
+    bandeira.innerHTML = "Euro";
+    trocaBandeira.src = "./assets/Euro.png";
+  }
 
-    if (selecteuro.value == "euro"){
-        bandeira.innerHTML = "Euro";
-        trocabandeira.src = "./assets/Euro.png"
-    }
+  ConvertValor();
+}
 
-
-    ConvertValor()
-
-} 
-convertbutton.addEventListener("click", ConvertValor);
-selecteuro.addEventListener("change", trocadevalor );
+convertButton.addEventListener("click", ConvertValor);
+selectEuro.addEventListener("change", trocaDeValor);
